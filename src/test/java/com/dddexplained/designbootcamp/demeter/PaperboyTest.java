3f466407bc3ed1f046f51d3b8c59b1834d901be9
zhangyi <brucezhang@tom.com>
2020-05-03 15:45:06 +0800

    let test pass

--- src/test/java/com/dddexplained/designbootcamp/demeter/PaperboyTest.java
+++ src/test/java/com/dddexplained/designbootcamp/demeter/PaperboyTest.java
@@ -2,10 +2,16 @@ package com.dddexplained.designbootcamp.demeter;
 
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PaperboyTest {
     @Test
     public void should_pay() {
         Paperboy paperboy = new Paperboy();
-        paperboy.charge(null, 100f);
+        Wallet wallet = new Wallet(1000f);
+        Customer customer = new Customer("bruce", "zhang", wallet);
+        paperboy.charge(customer, 100f);
+
+        assertThat(wallet.getTotalMoney()).isEqualTo(900f);
     }
 }
